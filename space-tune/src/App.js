cef3e4330cb3595b02485b0e98bd6553bf8ba21a
CivicPie <kent_2015@outlook.com>
2024-04-06 14:25:32 -0400

    Create home page

--- space-tune/src/App.js
+++ space-tune/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import {Home} from './Components/Home.js'
 import Review from './Components/Review';
 import Spotify from './Spotify'
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       <Spotify/>
+      <Review/>
     </div>
   );
 }
