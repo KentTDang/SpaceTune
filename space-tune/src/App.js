fb2cf315c1a5c615477035e894caf03c0e025670
CivicPie <kent_2015@outlook.com>
2024-04-06 17:16:16 -0400

    Update Styling

--- space-tune/src/App.js
+++ space-tune/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {NavBar} from './Components/NavBar.js'
 import { Banner } from './Components/Banner.js'
 import { Song } from './Components/Song.js'
-import Review from './Components/Review';
+import Review from './Components/Review.jsx';
 import Spotify from './Spotify'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -14,6 +14,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Song/>
+      <Review/>
       {/* <Spotify/>
       <Review/> */}
     </div>
--- space-tune/src/Components/Song.js
+++ space-tune/src/Components/Song.js
@@ -78,6 +78,155 @@ export const Song = () => {
                 <SwiperSlide>
                   <img src={headerImg} alt="slide_image"/>
                 </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+
+                <div className="slider-container">
+                  <div className="swiper-button-prev slider-arrow">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                  </div>
+                  <div className="swiper-button-next slider-arrow">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                  </div>
+                </div>
+              </Swiper>
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col>
+            <div className="song-bx1">
+              <h2>
+                Trending Songs
+              </h2>
+              <p>FAT FAT FAT FAT FATFA FATFTGSYTBXUBWUYBXU</p>
+              <Swiper 
+              effect={'coverflow'} 
+              grabCursor={true} 
+              centeredSlides={true} 
+              loop={true} 
+              slidesPerView={'auto'} 
+              coverflowEffect={
+                {
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 2.5
+                }
+              }
+              paginatio={{el:'.swiper-pagination',clickable:true}}
+              navigation={{
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev',
+                clickable: true,
+              }}
+              modules={[EffectCoverflow,Pagination,Navigation]} 
+              className='swiper_container'>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+
+                <div className="slider-container">
+                  <div className="swiper-button-prev slider-arrow">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                  </div>
+                  <div className="swiper-button-next slider-arrow">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                  </div>
+                </div>
+              </Swiper>
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col>
+            <div className="song-bx1">
+              <h2>
+                Trending Songs
+              </h2>
+              <p>FAT FAT FAT FAT FATFA FATFTGSYTBXUBWUYBXU</p>
+              <Swiper 
+              effect={'coverflow'} 
+              grabCursor={true} 
+              centeredSlides={true} 
+              loop={true} 
+              slidesPerView={'auto'} 
+              coverflowEffect={
+                {
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 2.5
+                }
+              }
+              paginatio={{el:'.swiper-pagination',clickable:true}}
+              navigation={{
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev',
+                clickable: true,
+              }}
+              modules={[EffectCoverflow,Pagination,Navigation]} 
+              className='swiper_container'>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
 
                 <div className="slider-container">
                   <div className="swiper-button-prev slider-arrow">
