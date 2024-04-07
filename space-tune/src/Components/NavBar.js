7060a8144a1d9ca951464d4f34948c5a7b2477dc
Rachel Le <lehrachel2004@gmail.com>
2024-04-07 07:18:07 -0400

    Implement Search Bar

--- space-tune/src/Components/NavBar.js
+++ space-tune/src/Components/NavBar.js
@@ -130,7 +130,7 @@ export const NavBar = () => {
             
           <button id = "submit-button" onClick={searchArtists}>SUBMIT</button>
             <input id = "songSearch" placeholder = "search.." type="text" onChange={e => setSearchKey(e.target.value)} />
-            {!token ?
+              {!token ?
                <a id = "login-button" href={`${AUTH_ENDPOINT}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`}>Login</a>
                 : <button id = "logout" onClick={logout}>Logout</button>                
             }
